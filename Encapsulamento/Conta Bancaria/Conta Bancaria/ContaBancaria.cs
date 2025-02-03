ff289b8e5e808b2496fe4c0f0423de065542ddbd
Pedro Paulo Carneiro Ferreira <158389145+DevProdigio@users.noreply.github.com>
2025-02-03 18:44:00 -0300

    Add files via upload

---
+++ Encapsulamento/Conta Bancaria/Conta Bancaria/ContaBancaria.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta_Bancaria
+{
+    class ContaBancaria
+    {
+        private decimal Saldo;
+
+        public void Sacar(decimal valor)
+        {
+            if (Saldo >= valor && valor > 0)
+            {
+                Saldo -= valor;
+                Console.WriteLine($"Saque no valor de {valor}$ efetuado com sucesso. Novo saldo {Saldo}$");
+            }
+            else if (Saldo < valor){
+                Console.WriteLine($"Você não tem saldo suficiente. Saldo atual {Saldo}$");
+
+            }
+            else
+            {
+                Console.WriteLine("Valor invalido!");
+            }
+        }
+
+        public void Depositar(decimal valor)
+        {
+            if (valor > 0)
+            {
+                Saldo += valor;
+                Console.WriteLine($"Deposito no valor de {valor}$ efetuado com suscesso. Novo saldo {Saldo}$");
+            }
+            else
+            {
+                Console.WriteLine("Valor do deposito invalido ou insuficiente.");
+            }
+        }
+
+        public void ConsultarSaldo()
+        {
+            Console.WriteLine($"Saldo Atual: {Saldo}$");
+        }
+    }
+}
---
+++ Encapsulamento/Conta Bancaria/Conta Bancaria/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Conta_Bancaria;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ContaBancaria conta = new ContaBancaria();
+        conta.Depositar(5000);
+        conta.Sacar(1000);
+        conta.ConsultarSaldo();
+    }
+}
